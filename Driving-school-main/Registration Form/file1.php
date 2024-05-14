69f847b74373f4399f9211a0e8e4b932777303de
Thanuja Dilshan <148608474+thanujadilshan16863@users.noreply.github.com>
2024-05-15 04:11:55 +0530

    Update file1.php
    td

--- Driving-school-main/Registration Form/file1.php
+++ Driving-school-main/Registration Form/file1.php
@@ -1,45 +1,45 @@
 <?php 
-     $fullname=$_POST['fullname'];
-     $fname=$_POST['fname'];
-     $lname=$_POST['lname'];
-     $bdate=$_POST['bdate'];
-     $emeil=$_POST['email'];
-     $mnumber=$_POST['mnumber'];
-     $gender=$_POST['gender'];
-     $occupation=$_POST['occupation'];
-     $idtype=$_POST['idtype'];
-     $idnumber=$_POST['idnumber'];
-     $isdate=$_POST['isdate'];
-     $address=$_POST['adress'];
-     $nationality=$_POST['nationality'];
-     $district=$_POST['district'];
+$fullname = $_POST['fullname'];
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$bdate = $_POST['bdate'];
+$email = $_POST['email'];
+$mnumber = $_POST['mnumber'];
+$gender = $_POST['gender'];
+$occupation = $_POST['occupation'];
+$idtype = $_POST['idtype'];
+$idnumber = $_POST['idnumber'];
+$isdate = $_POST['isdate'];
+$address = $_POST['address'];
+$nationality = $_POST['nationality'];
+$district = $_POST['district'];
 
-     if($gender=='male'){
-        $ngender='male';
-     }
-     if else($gender=='female'){
-        $ngender='female';
-     }
-     else{
-        $ngender='other';
-     }
+// Assigning gender
+if ($gender == 'Male') {
+    $ngender = 'male';
+} elseif ($gender == 'Female') {
+    $ngender = 'female';
+} else {
+    $ngender = 'other';
+}
 
-     if($idtype=='new'){
-        $nidtype='new';
-     }
-     else{
-        $nidtype='old';
-    }
+// Assigning ID type
+if ($idtype == 'new') {
+    $nidtype = 'new';
+} else {
+    $nidtype = 'old';
+}
 
-    $conn=new mysql('localhost','root','','test');
-    if($conn->connect_error){
-        die('Connection Failed :'.$conn->connect_error);
-    }else{
-        $stmt=$conn->prepare("insert into customer(Full_Name,First_Name,Last_Name,Birth_Date,Email,Mob_Number,Gender,Occupation,ID_Type,ID_Number,Issued_Date,Address,Nationality,District) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-        $stmt->bind_param("sssssi",$fullname,$fname,$lname,$bdate,$emeil,$mnumber,$gender,$occupation,$idtype,$idnumber,$isdate,$address,$nationality,$district);
-        $stmt->execute();
-        echo "Registration Successfull.....";
-        $stmt->close();
-        $conn->close();
-    }
+// Create MySQL connection
+$conn = new mysqli('localhost', 'root', '', 'test');
+if ($conn->connect_error) {
+    die('Connection Failed: ' . $conn->connect_error);
+} else {
+    $stmt = $conn->prepare("INSERT INTO customer(Full_Name, First_Name, Last_Name, Birth_Date, Email, Mob_Number, Gender, Occupation, ID_Type, ID_Number, Issued_Date, Address, Nationality, District) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssssssssss", $fullname, $fname, $lname, $bdate, $email, $mnumber, $ngender, $occupation, $nidtype, $idnumber, $isdate, $address, $nationality, $district);
+    $stmt->execute();
+    echo "Registration Successful.....";
+    $stmt->close();
+    $conn->close();
+}
 ?>
